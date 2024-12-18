61208c77e2cef3513141f84e228d691b042e2159
pardeepdeveloper7 <you@example.com>
2024-12-18 15:42:56 +0530

    code merge

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,9 +1,13 @@
-import React from 'react'
+import mysql from 'mysql2/promise';
+import dotenv from 'dotenv';
 
-const db = () => {
-  return (
-    <div>db</div>
-  )
-}
+dotenv.config();
 
-export default db
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+export default pool;

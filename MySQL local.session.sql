68f5702324960a58ad38fc412de10b2684f5b069
officialdataground <official.dataground@gmail.com>
2024-12-18 19:01:49 +0530

    database

---
+++ MySQL local.session.sql
@@ -0,0 +1,12 @@
+CREATE TABLE Users_login (
+
+
+    login VARCHAR(50) NOT NULL,         
+    password VARCHAR(255) NOT NULL      
+);
+INSERT into Users_login (Users_login,(username, password) values
+('ram', '123'),
+('sam ','123' ));
+show database;
+
+

9bf6e8a06b5082b031286bb367de261b5e93e4fd
pardeepdeveloper7 <you@example.com>
2024-12-18 15:48:50 +0530

    code merge

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -4,9 +4,9 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOSTsdsdv,
+  user: process.env.DB_USERghgjgh,
+  password: process.env.DB_PASSWORDfnmskdfnml,
   database: process.env.DB_NAME,
 });
 
